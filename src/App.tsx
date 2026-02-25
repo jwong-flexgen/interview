0f05c0d7c9cae9756f96bba6aefe0ffcb0a1b38b
Jacky Wong <jwong@flexgen.com>
2026-02-25 15:30:48 -0500

    new start point with shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className='flex flex-col items-center justify-center gap-2'>
+      <h1 className='font-bold'>Hello World</h1>
+      <Button variant='outline'>Click</Button>
+    </div>
+  )
+}
+
+export default App
